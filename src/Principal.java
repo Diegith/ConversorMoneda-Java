7a54646f223f5a9d960a0974e5afda5226e79bb4
Diegith <diego.albarracinacero@gmail.com>
2025-06-07 09:00:53 -0500

    Principal Update -  Concatenate

--- src/Principal.java
+++ src/Principal.java
@@ -5,15 +5,21 @@ public class Principal {
         ConsultaAPI consultaAPI = new ConsultaAPI();
         CrearArchivo crearArchivo = new CrearArchivo();
         Menu menu = new Menu();
+        
         int opcion = menu.obtenerOpcion();
-        Double valorAConvertir = menu.obtenerValorAConvertir();
+        double valorAConvertir = menu.obtenerValorAConvertir();
+
         String conversor = menu.tipoConversor(opcion);
         String[] monedas = conversor.split("/");
+
         String consultaJson = consultaAPI.obtenerDatos(conversor);
         Consulta consulta = crearArchivo.JsonToConsulta(consultaJson);
+
         crearArchivo.GuardarEnJson(consulta);
+
         double tasaDeConversion = consulta.conversion_rate();
         double valorCovertido = valorAConvertir * tasaDeConversion;
+
         System.out.println("El valor " + valorAConvertir + " " + monedas[0] + " al valor final de =>>> " + valorCovertido + " " + monedas[1]);
     }
 }
